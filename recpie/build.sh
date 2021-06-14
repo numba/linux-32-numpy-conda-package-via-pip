19bbbf970beb1657a9bf9628257519b65920bf04
Valentin Haenel <vhaenel@anaconda.com>
2021-06-14 16:29:57 +0200

    fixes need for successful build
    As title

--- recpie/build.sh
+++ recpie/build.sh
@@ -1,1 +1,3 @@
-pip install numpy==1.17.5
+echo "trying pip install"
+pip install https://files.pythonhosted.org/packages/48/48/786a91a51e0b123485f575ee9a775b5519afe06c759f4e99faeb65f22a0c/numpy-1.17.5-cp37-cp37m-manylinux1_i686.whl
+
